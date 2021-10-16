49399237456e43ff95aa259d1d3f9a82a18395f6
David Ezekiel <davidlutta2010@gmail.com>
2021-10-16 11:30:09 +0300

    Add dht11 and MQ2 sensor files
    This commit adds files to read sensor values

--- rpi/sensors/dht11.js
+++ rpi/sensors/dht11.js
@@ -1,5 +1,9 @@
 const sensor = require("node-dht-sensor")
-
+/**
+ * This method fetches the temperature and humidity values
+ * from the dht11 sensor
+ * @returns Temperature & Humidity values
+ */
 async function getTemperatureAndHumidity(){
     try {
         const res = await sensor.read(11, 4);
