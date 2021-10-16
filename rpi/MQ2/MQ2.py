c936f94c9d1792bc810d0d87d5cbbb30c3540aa9
David Lutta <davidlutta2010@gmail.com>
2021-10-16 19:31:57 +0300

    Add module for MQ2 sensor

---
+++ rpi/MQ2/MQ2.py
@@ -0,0 +1,44 @@
+import serial
+import location as location
+import smtplib
+import time 
+
+password = input("Type your password and press enter: ")
+
+sender = ''
+receivers = ['']
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender, password)
+print("Login success")
+
+'''
+    This method gets the smoke sensor values from the MQ2 smoke sensor
+'''
+def get_smoke_sensor_value():
+    ser = serial.Serial('/dev/ttyUSB0',
+                        baudrate=115200)
+
+    return ser.readline()[0]
+
+def sendEmail(sender, receivers, message):
+    try:
+        server.sendmail(sender, receivers, message)         
+        print("Successfully sent email")
+    except smtplib.SMTPException:
+        print("Error: unable to send email")
+
+
+while True:
+    smoke_sensor_val = get_smoke_sensor_value()
+    gps = location.get_gps_coordinates()
+    if(smoke_sensor_val > 50):
+        message = """
+        Subject: Smoke Detected in Forest!
+
+        Smoke detected at gps coordinates {val}
+        """.format(val=gps)
+        sendEmail(sender, receivers, message)
+    time.sleep(5)
+    
