6827986a5cbc85576fb23c50cac2245ea24622d5
radaMoi <radamuapp@gmail.com>
2021-10-16 14:43:28 +0300

    dashboard

---
+++ dashboard/src/routes.js
@@ -0,0 +1,64 @@
+/*!
+
+=========================================================
+* Now UI Dashboard React - v1.5.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/TableList.js";
+import Maps from "views/Maps.js";
+import Upgrade from "views/Upgrade.js";
+import UserPage from "views/UserPage.js";
+
+var dashRoutes = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: "design_app",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/alerts",
+    name: "Alerts",
+    icon: "ui-1_bell-53",
+    component: Icons,
+    layout: "/admin",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "location_map-big",
+    component: Maps,
+    layout: "/admin",
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    icon: "loader_gear",
+    component: Notifications,
+    layout: "/admin",
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    icon: "files_paper",
+    component: TableList,
+    layout: "/admin",
+  }
+];
+export default dashRoutes;
