d43bf3ea5b95bd939178cfe1af4202d16b7b511f
David Lutta <davidlutta2010@gmail.com>
2021-10-17 19:24:45 +0300

    Update location and MQ2 modules to send data to azure iot hub

--- rpi/MQ2/MQ2.py
+++ rpi/MQ2/MQ2.py
@@ -1,17 +1,31 @@
 import serial
-import location as location
 import smtplib
-import time 
+import time
+import uuid
+import json
+from azure.iot.device import IoTHubDeviceClient, Message
 
-password = input("Type your password and press enter: ")
-
+password = ""
 sender = ''
 receivers = ['']
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender, password)
-print("Login success")
+azure_connection_string = ""
+MSG_TXT = '{{"messageId":{messageId}, "deviceId": "RasPiTemp", "C0Value": {val}}}'
+
+def mail_server_init():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, password)
+    print("Login success")
+    return server
+
+def get_gps_coordinates():
+    f = open("/home/pi/Desktop/projects/msft-hackathon/rpi/location/location.txt")
+    return f.read()
+
+def iothub_client_init():
+    client = IoTHubDeviceClient.create_from_connection_string(azure_connection_string)
+    return client
 
 '''
     This method gets the smoke sensor values from the MQ2 smoke sensor
@@ -19,26 +33,41 @@ print("Login success")
 def get_smoke_sensor_value():
     ser = serial.Serial('/dev/ttyUSB0',
                         baudrate=115200)
-
     return ser.readline()[0]
 
 def sendEmail(sender, receivers, message):
     try:
+        server = mail_server_init()
         server.sendmail(sender, receivers, message)         
         print("Successfully sent email")
     except smtplib.SMTPException:
         print("Error: unable to send email")
 
+def post_smoke_sensor_value_to_azure_iot(smoke_sensor_val):
+    try:
+        client = iothub_client_init()
+        formatted_message = MSG_TXT.format(messageId=uuid.uuid4(),val=smoke_sensor_val)
+        message = Message(formatted_message)
+        print("Sending Message: {}".format(message))
+        client.send_message(message)
+        print("Message Successfully sent!")
+    except:
+        print("An Error ocurred while sending data to iot hub")
 
 while True:
     smoke_sensor_val = get_smoke_sensor_value()
-    gps = location.get_gps_coordinates()
+    print(smoke_sensor_val)
+    gps = get_gps_coordinates()
+    jsonGPS = json.loads(gps)
+    lat = str(jsonGPS['lat'])
+    lng = str(jsonGPS['lng'])
+    post_smoke_sensor_value_to_azure_iot(smoke_sensor_val)
     if(smoke_sensor_val > 100):
         message = """
         Subject: Smoke Detected in Forest!
 
-        Smoke detected at gps coordinates {val}
-        """.format(val=gps)
+        Smoke detected at gps coordinates Latitude:{lat} Longitude:{lng}
+        """.format(lat=lat, lng=lng)
+        print(message)
         sendEmail(sender, receivers, message)
-    time.sleep(5)
-    
+    time.sleep(6000) # wait for 10 minutes before next reading
